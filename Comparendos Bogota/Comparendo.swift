3668971424d5b0c848e31ba1727a13aeb78e8dd1
Daniel Warner <danywarner@gmail.com>
2016-01-25 15:57:29 -0500

    comparendos created through Firebase

---
+++ Comparendos Bogota/Comparendo.swift
@@ -0,0 +1,99 @@
+//
+//  Comparendo.swift
+//  Comparendos Bogota
+//
+//  Created by Daniel Warner on 1/25/16.
+//  Copyright © 2016 Daniel Warner. All rights reserved.
+//
+
+import Foundation
+
+
+class Comparendo {
+    private var _code: String!
+    private var _description: String!
+    private var _SMDLVValue: Int!
+    private var _towedAway: Bool?
+    
+    var code: String {
+        get {
+            return _code
+        }
+        set {
+            _code = newValue
+        }
+    }
+    
+    var description: String! {
+        get {
+            if _description == nil {
+                _description = ""
+            }
+            
+            return _description
+        }
+        set {
+            _description = newValue
+        }
+    }
+    
+    var SMDLVValue: Int! {
+        get {
+            if _SMDLVValue == nil {
+                _SMDLVValue = 0
+            }
+            return _SMDLVValue!
+        }
+    }
+    
+    var towedAway: Bool! {
+        get {
+            if _towedAway == nil {
+                _towedAway = false
+            }
+            return _towedAway!
+        }
+    }
+    
+    
+    init(code: String) {
+        _code = code
+    }
+    
+    init(code: String, dictionary: Dictionary<String, AnyObject>) {
+        self._code = code
+        
+        _description = ""
+        _SMDLVValue = 0
+        _towedAway = false
+        
+         print(dictionary.debugDescription)
+        if let code = dictionary["codigo"] as? String {
+            
+                _code = code
+        }
+        
+        if let description = dictionary["descripcion"] as? String {
+            
+            _description = description
+        }
+        
+        
+        
+        if let SMDLVValue = dictionary["valorSMDLV"] as? Int {
+            
+            _SMDLVValue = SMDLVValue
+        }
+        
+        if let towedAway = dictionary["inmovilizable"] as? Int {
+            
+            if towedAway == 0 {
+                _towedAway = false
+            } else if towedAway == 1 {
+                _towedAway = true
+            }
+        }
+        
+    }
+    
+}
--- Comparendos Bogota/ViewController.swift
+++ Comparendos Bogota/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     var SMDLV = 0
     var SMMLV = 0
-    
+    var comparendos = [Comparendo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,24 @@ class ViewController: UIViewController {
                 self.SMDLV = salariosDict["SMDLV"] as! Int
                 self.SMMLV = salariosDict["SMMLV"] as! Int
             }
+        })
+        
+        DataService.ds.REF_COMPARENDOS.observeEventType(.Value, withBlock: { snapshot in
+            
+            self.comparendos = []
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                for snap in snapshots {
+                    
+                    if let comparendoDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let comparendo = Comparendo(code: key, dictionary: comparendoDict)
+                        self.comparendos.append(comparendo)
+                        
+                    }
+                }
+            }
+           // self.collection.reloadData()
         })
         
         
