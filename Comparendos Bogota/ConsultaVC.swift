cd2b02e12e1d9d01bce1cc728b797bdfdd0f6c39
Daniel Warner <danywarner@gmail.com>
2016-01-27 12:30:58 -0500

    keyboard types set + parametric search working with textfield and picker

--- Comparendos Bogota/ConsultaVC.swift
+++ Comparendos Bogota/ConsultaVC.swift
@@ -9,24 +9,71 @@
 import UIKit
 import Alamofire
 
-class ConsultaVC: UIViewController {
+class ConsultaVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
     
+    @IBOutlet weak var DocPicker: UIPickerView!
     @IBOutlet weak var textField: UITextField!
     typealias DownloadComplete = () -> ()
     
-    var url2 = "http://consultas.transitobogota.gov.co:8083/consultas_generales/buscar_comparendos.php?datos_enviados=S&tipo_documento=1&numero_identificacion=&placa_veh=gld591&tipo_busqueda=BC&existe_financiacion_finan=0&existe_financiacion_acpag=0"
+    var baseString = "http://consultas.transitobogota.gov.co:8083/consultas_generales/buscar_comparendos.php?"
+    var datosEnviados = "datos_enviados=S"
+    var tipoDocumento = "tipo_documento="
+    var numeroIdentificacion = "numero_identificacion="
+    var placaVeh = "placa_veh="
+    var tipoBusqueda = "tipo_busqueda=BC"
+    var urlString = ""
+    let pickerData = [["Cédula de ciudadanía", "Placa Vehículo", "N.I.T.", "Cédula de extranjería","Tarjeta de identidad", "Pasaporte","Registro civil","Servitecas", "Carnet diplomático"]]
     
-    var url1 = "http://consultas.transitobogota.gov.co:8083/consultas_generales/buscar_comparendos.php?datos_enviados=S&tipo_documento=1&numero_identificacion=1014192693&placa_veh=&pagina_actual=1&tipo_busqueda=BC&existe_financiacion_finan=0&existe_financiacion_acpag=0"
+    enum documentTypes:Int{
+        case cedulaCiudadania = 1
+        case nit = 2
+        case cedulaExtranjeria = 3
+        case tarjetaIdentidad = 4
+        case pasaporte = 5
+        case registroCivil = 6
+        case servitecas = 7
+        case carnetDiplomatico = 8
+    }
     
+//    var url1 = "http://consultas.transitobogota.gov.co:8083/consultas_generales/buscar_comparendos.php?datos_enviados=S&tipo_documento=1&numero_identificacion=1014192693&placa_veh=&pagina_actual=1&tipo_busqueda=BC&existe_financiacion_finan=0&existe_financiacion_acpag=0"
+//    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        DocPicker.delegate = self
+        DocPicker.dataSource = self
+        textField.delegate = self
+        DocPicker.selectRow(0, inComponent: 0, animated: false)
+        printPickerData()
     }
 
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
     
     
     
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let docTypesArray = pickerData[0]
+        if docTypesArray.indexOf(pickerData[0][DocPicker.selectedRowInComponent(0)]) == 1 {
+            textField.keyboardType = UIKeyboardType.Alphabet
+        } else {
+            textField.keyboardType = UIKeyboardType.NumbersAndPunctuation
+        }
+        
+        printPickerData()
+    }
     
+    func printPickerData() {
+        print(pickerData[0][DocPicker.selectedRowInComponent(0)])
+    }
     
     @IBAction func queryButtonPressed(sender: AnyObject) {
         
@@ -37,17 +84,51 @@ class ConsultaVC: UIViewController {
     }
     
     func queryForComparendos(completed: DownloadComplete) {
-        let url = NSURL(string: url1)!
+        let docTypesArray = pickerData[0]
+        placaVeh = "placa_veh="
+        tipoDocumento = "tipo_documento="
+        numeroIdentificacion = "numero_identificacion="
+        
+        
+        let index = docTypesArray.indexOf(pickerData[0][DocPicker.selectedRowInComponent(0)])! + 1
+        let indexStr = String(index)
+        
+        
+        if index != 2 {
+            if let document = textField.text {
+                tipoDocumento.appendContentsOf(indexStr)
+                numeroIdentificacion.appendContentsOf(document)
+            }
+            
+        } else {
+            if let placa = textField.text{
+                
+                let trimmedString = placa.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+                placaVeh.appendContentsOf(trimmedString)
+            }
+        }
+        
+        
+        
+        urlString = "\(baseString)\(datosEnviados)&\(tipoDocumento)&\(numeroIdentificacion)&\(placaVeh)&\(tipoBusqueda)"
+        print(urlString)
+        let url = NSURL(string: urlString)!
         Alamofire.request(.GET, url).responseString { (response) -> Void in
             let result = response.result
-            if result.value.debugDescription.rangeOfString("VIGENTE") != nil{
-                print("hay uno!")
+            if result.value.debugDescription.lowercaseString.rangeOfString("no se encontraron registros de comparendos") != nil{
+                print("no hay!")
             } else {
-                print("no hay")
+                print("tiene comparendos")
             }
            // print(result.value.debugDescription)
         }
         
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        queryButtonPressed(self.view)
+        return false
     }
     
     @IBAction func backBtnPressed(sender: AnyObject) {
